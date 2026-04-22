782af3025fd5fb6f5ff720bc70b6604bea597da1
Kerem-ozcan <keremo@stu.aydin.edu.tr>
2026-04-22 10:31:58 +0300

    SQL Fix and adding user_id into SQL

--- EventClass.py
+++ EventClass.py
@@ -2,7 +2,8 @@ import sqlite3
 
 
 class Event:
-    def __init__(self, title, description, date, start_time, end_time, id=None):
+    def __init__(self, User_id,title, description, date, start_time, end_time, id=None):
+        self.User_id = User_id 
         self.id = id
         self.title = title
         self.description = description
@@ -15,11 +16,11 @@ class Event:
         return sqlite3.connect('student_checking.db')
 
     @staticmethod
-    def add_event(title, description, date, start_time, end_time):
+    def add_event(user_id, id, title, description, date, start_time, end_time):
         conn = Event.get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO events (title, description, date, start_time, end_time) VALUES (?, ?, ?, ?, ?)',
-                      (title, description, date, start_time, end_time))
+        cursor.execute('INSERT INTO events (id, title, description, date, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?)',
+                      (user_id, id, title, description, date, start_time, end_time))
         conn.commit()
         conn.close()
 
@@ -31,3 +32,11 @@ class Event:
         events = cursor.fetchall()
         conn.close()
         return events
+        @staticmethod
+        def get_event_with_user_id(User_id): 
+            conn = Event.get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM events WHERE user_id = ?', (user_id,))
+            events = cursor.fetchall()
+            conn.close()
+            return events
